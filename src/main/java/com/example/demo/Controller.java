090af4ad9886ceb9bffc1aa8127f1b9befaaacee
Jaspal <jasdhali@gmail.com>
2024-02-04 00:32:07 -0600

    change

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -4,16 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.azure.cosmos.CosmosAsyncContainer;
 import com.azure.cosmos.CosmosClientBuilder;
+import com.azure.cosmos.models.CosmosItemResponse;
 import com.azure.cosmos.models.CosmosQueryRequestOptions;
 import com.azure.cosmos.models.FeedResponse;
 
 import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RequestMapping("/cosmos")
 @RestController
@@ -41,6 +45,12 @@ public class Controller {
 		CosmosQueryRequestOptions options = new CosmosQueryRequestOptions();
 		return container.queryItems("SELECT TOP 20 * FROM City c", options, City.class).byPage()
 				.map(FeedResponse::getResults);
+	}
+
+	@PostMapping("/cities")
+	public String createCities(@RequestBody City city) {		
+		Mono<CosmosItemResponse<City>> mono =  container.createItem(city);
+		return "City Created";
 	}
 
 	@GetMapping("/hello")
